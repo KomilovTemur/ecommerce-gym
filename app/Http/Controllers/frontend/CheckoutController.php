1526761fbc89fa2c8eb997c17c5ec80f4d4f0a60
Temurbek Komilov <itteamdev74@gmail.com>
2023-06-05 14:27:45 +0500

    some changes

--- app/Http/Controllers/frontend/CheckoutController.php
+++ app/Http/Controllers/frontend/CheckoutController.php
@@ -15,21 +15,20 @@ class CheckoutController extends Controller
 {
     public function index()
     {
-        $old_cartitem=Cart::where('user_id',Auth::id())->get();
-        foreach($old_cartitem as $item)
-        {
-            if(!Product::where('id',$item->prod_id)->where('qty' , '>=' , $item->prod_qty)->exists())
-            {
-                $removeItem = Cart::where('user_id',Auth::id())->where('prod_id',$item->prod_id)->first();
+        $old_cartitem = Cart::where('user_id', Auth::id())->get();
+        foreach ($old_cartitem as $item) {
+            if (!Product::where('id', $item->prod_id)->where('qty', '>=', $item->prod_qty)->exists()) {
+                $removeItem = Cart::where('user_id', Auth::id())->where('prod_id', $item->prod_id)->first();
                 $removeItem->delete();
             }
         }
-        $cartitem=Cart::where('user_id',Auth::id())->get();
-        return view('frontend.checkout' , compact('cartitem'));
+        $cartitem = Cart::where('user_id', Auth::id())->get();
+        return view('frontend.checkout', compact('cartitem'));
     }
 
     public function placeOrder(Request $request)
     {
+        // dd($request->fname);
         $order  = new Order();
         $order->user_id = Auth::id();
         $order->fname = $request->input('fname');
@@ -42,24 +41,23 @@ class CheckoutController extends Controller
         $order->state = $request->input('state');
         $order->country = $request->input('country');
         $order->pincode = $request->input('pincode');
-      
+
         $total = 0;
-        $cartitems_total = Cart::where('user_id',Auth::id())->get();
+        $cartitems_total = Cart::where('user_id', Auth::id())->get();
         foreach ($cartitems_total as $prod) {
             $total += $prod->products->selling_price * $prod->prod_qty;
         }
 
-        $order->total_price= $total;
+        $order->total_price = $total;
 
-        $order->tracking_no = 'AbdullahMoiz'.rand(1111,9999);
+        $order->tracking_no = 'ecomerce' . rand(1111, 9999);
         $order->save();
 
         $order->id;
 
-        $cartitem=Cart::where('user_id',Auth::id())->get();
+        $cartitem = Cart::where('user_id', Auth::id())->get();
 
-        foreach($cartitem as $item)
-        {
+        foreach ($cartitem as $item) {
             OrderItem::create([
                 'order_id' => $order->id,
                 'prod_id' => $item->prod_id,
@@ -67,14 +65,13 @@ class CheckoutController extends Controller
                 'price' => $item->products->selling_price,
             ]);
 
-            $prod  = Product::where('id' , $item->prod_id)->first();
+            $prod  = Product::where('id', $item->prod_id)->first();
             $prod->qty = $prod->qty - $item->prod_qty;
             $prod->update();
         }
 
-        if(Auth::user()->address1 == NULL)
-        {
-            $user = User::where('id',Auth::id())->first();
+        if (Auth::user()->address1 == NULL) {
+            $user = User::where('id', Auth::id())->first();
             $user->lname = $request->input('lname');
             $user->phoneno = $request->input('phoneno');
             $user->address1 = $request->input('address1');
@@ -82,13 +79,13 @@ class CheckoutController extends Controller
             $user->city = $request->input('city');
             $user->state = $request->input('state');
             $user->country = $request->input('country');
-            $user->pincode = $request->input('pincode');   
+            $user->pincode = $request->input('pincode');
             $user->update();
         }
-        $cartitems = Cart::where('user_id',Auth::id());
+        $cartitems = Cart::where('user_id', Auth::id());
         Cart::destroy($cartitem);
 
-        
-        return redirect('/')->with('status',"Order Placed successfully");
+
+        return redirect('/')->with('status', "Order Placed successfully");
     }
 }
